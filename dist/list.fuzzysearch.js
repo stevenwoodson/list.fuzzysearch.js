cd01e74104f2cf7e1977493942aa5a9c0c6e3ab2
Steven Woodson <stevenwoodson@gmail.com>
2017-01-14 15:36:27 -0600

    "component/jquery" was deprecated in favor of "components/jquery", changed and rebuilt library

--- dist/list.fuzzysearch.js
+++ dist/list.fuzzysearch.js
@@ -1,43 +1,108 @@
+
 ;(function(){
 
 /**
- * Require the given path.
+ * Require the module at `name`.
  *
- * @param {String} path
+ * @param {String} name
  * @return {Object} exports
  * @api public
  */
 
-function require(path, parent, orig) {
-  var resolved = require.resolve(path);
+function require(name) {
+  var module = require.modules[name];
+  if (!module) throw new Error('failed to require "' + name + '"');
 
-  // lookup failed
-  if (null == resolved) {
-    orig = orig || path;
-    parent = parent || 'root';
-    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
-    err.path = orig;
-    err.parent = parent;
-    err.require = true;
-    throw err;
-  }
-
-  var module = require.modules[resolved];
-
-  // perform real require()
-  // by invoking the module's
-  // registered function
-  if (!module._resolving && !module.exports) {
-    var mod = {};
-    mod.exports = {};
-    mod.client = mod.component = true;
-    module._resolving = true;
-    module.call(this, mod.exports, require.relative(resolved), mod);
-    delete module._resolving;
-    module.exports = mod.exports;
+  if (!('exports' in module) && typeof module.definition === 'function') {
+    module.client = module.component = true;
+    module.definition.call(this, module.exports = {}, module);
+    delete module.definition;
   }
 
   return module.exports;
+}
+
+/**
+ * Meta info, accessible in the global scope unless you use AMD option.
+ */
+
+require.loader = 'component';
+
+/**
+ * Internal helper object, contains a sorting function for semantiv versioning
+ */
+require.helper = {};
+require.helper.semVerSort = function(a, b) {
+  var aArray = a.version.split('.');
+  var bArray = b.version.split('.');
+  for (var i=0; i<aArray.length; ++i) {
+    var aInt = parseInt(aArray[i], 10);
+    var bInt = parseInt(bArray[i], 10);
+    if (aInt === bInt) {
+      var aLex = aArray[i].substr((""+aInt).length);
+      var bLex = bArray[i].substr((""+bInt).length);
+      if (aLex === '' && bLex !== '') return 1;
+      if (aLex !== '' && bLex === '') return -1;
+      if (aLex !== '' && bLex !== '') return aLex > bLex ? 1 : -1;
+      continue;
+    } else if (aInt > bInt) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+  return 0;
+}
+
+/**
+ * Find and require a module which name starts with the provided name.
+ * If multiple modules exists, the highest semver is used. 
+ * This function can only be used for remote dependencies.
+
+ * @param {String} name - module name: `user~repo`
+ * @param {Boolean} returnPath - returns the canonical require path if true, 
+ *                               otherwise it returns the epxorted module
+ */
+require.latest = function (name, returnPath) {
+  function showError(name) {
+    throw new Error('failed to find latest module of "' + name + '"');
+  }
+  // only remotes with semvers, ignore local files conataining a '/'
+  var versionRegexp = /(.*)~(.*)@v?(\d+\.\d+\.\d+[^\/]*)$/;
+  var remoteRegexp = /(.*)~(.*)/;
+  if (!remoteRegexp.test(name)) showError(name);
+  var moduleNames = Object.keys(require.modules);
+  var semVerCandidates = [];
+  var otherCandidates = []; // for instance: name of the git branch
+  for (var i=0; i<moduleNames.length; i++) {
+    var moduleName = moduleNames[i];
+    if (new RegExp(name + '@').test(moduleName)) {
+        var version = moduleName.substr(name.length+1);
+        var semVerMatch = versionRegexp.exec(moduleName);
+        if (semVerMatch != null) {
+          semVerCandidates.push({version: version, name: moduleName});
+        } else {
+          otherCandidates.push({version: version, name: moduleName});
+        } 
+    }
+  }
+  if (semVerCandidates.concat(otherCandidates).length === 0) {
+    showError(name);
+  }
+  if (semVerCandidates.length > 0) {
+    var module = semVerCandidates.sort(require.helper.semVerSort).pop().name;
+    if (returnPath === true) {
+      return module;
+    }
+    return require(module);
+  }
+  // if the build contains more than one branch of the same module
+  // you should not use this funciton
+  var module = otherCandidates.sort(function(a, b) {return a.name > b.name})[0].name;
+  if (returnPath === true) {
+    return module;
+  }
+  return require(module);
 }
 
 /**
@@ -47,160 +112,33 @@ function require(path, parent, orig) {
 require.modules = {};
 
 /**
- * Registered aliases.
- */
-
-require.aliases = {};
-
-/**
- * Resolve `path`.
+ * Register module at `name` with callback `definition`.
  *
- * Lookup:
- *
- *   - PATH/index.js
- *   - PATH.js
- *   - PATH
- *
- * @param {String} path
- * @return {String} path or null
- * @api private
- */
-
-require.resolve = function(path) {
-  if (path.charAt(0) === '/') path = path.slice(1);
-
-  var paths = [
-    path,
-    path + '.js',
-    path + '.json',
-    path + '/index.js',
-    path + '/index.json'
-  ];
-
-  for (var i = 0; i < paths.length; i++) {
-    var path = paths[i];
-    if (require.modules.hasOwnProperty(path)) return path;
-    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
-  }
-};
-
-/**
- * Normalize `path` relative to the current path.
- *
- * @param {String} curr
- * @param {String} path
- * @return {String}
- * @api private
- */
-
-require.normalize = function(curr, path) {
-  var segs = [];
-
-  if ('.' != path.charAt(0)) return path;
-
-  curr = curr.split('/');
-  path = path.split('/');
-
-  for (var i = 0; i < path.length; ++i) {
-    if ('..' == path[i]) {
-      curr.pop();
-    } else if ('.' != path[i] && '' != path[i]) {
-      segs.push(path[i]);
-    }
-  }
-
-  return curr.concat(segs).join('/');
-};
-
-/**
- * Register module at `path` with callback `definition`.
- *
- * @param {String} path
+ * @param {String} name
  * @param {Function} definition
  * @api private
  */
 
-require.register = function(path, definition) {
-  require.modules[path] = definition;
+require.register = function (name, definition) {
+  require.modules[name] = {
+    definition: definition
+  };
 };
 
 /**
- * Alias a module definition.
+ * Define a module's exports immediately with `exports`.
  *
- * @param {String} from
- * @param {String} to
+ * @param {String} name
+ * @param {Generic} exports
  * @api private
  */
 
-require.alias = function(from, to) {
-  if (!require.modules.hasOwnProperty(from)) {
-    throw new Error('Failed to alias "' + from + '", it does not exist');
-  }
-  require.aliases[to] = from;
-};
-
-/**
- * Return a require function relative to the `parent` path.
- *
- * @param {String} parent
- * @return {Function}
- * @api private
- */
-
-require.relative = function(parent) {
-  var p = require.normalize(parent, '..');
-
-  /**
-   * lastIndexOf helper.
-   */
-
-  function lastIndexOf(arr, obj) {
-    var i = arr.length;
-    while (i--) {
-      if (arr[i] === obj) return i;
-    }
-    return -1;
-  }
-
-  /**
-   * The relative require() itself.
-   */
-
-  function localRequire(path) {
-    var resolved = localRequire.resolve(path);
-    return require(resolved, parent, path);
-  }
-
-  /**
-   * Resolve relative to the parent.
-   */
-
-  localRequire.resolve = function(path) {
-    var c = path.charAt(0);
-    if ('/' == c) return path.slice(1);
-    if ('.' == c) return require.normalize(p, path);
-
-    // resolve deps by returning
-    // the dep in the nearest "deps"
-    // directory
-    var segs = parent.split('/');
-    var i = lastIndexOf(segs, 'deps') + 1;
-    if (!i) i = 0;
-    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
-    return path;
+require.define = function (name, exports) {
+  require.modules[name] = {
+    exports: exports
   };
-
-  /**
-   * Check if module is defined at `path`.
-   */
-
-  localRequire.exists = function(path) {
-    return require.modules.hasOwnProperty(localRequire.resolve(path));
-  };
-
-  return localRequire;
 };
-require.register("component-indexof/index.js", function(exports, require, module){
+require.register("component~indexof@0.0.3", function (exports, module) {
 module.exports = function(arr, obj){
   if (arr.indexOf) return arr.indexOf(obj);
   for (var i = 0; i < arr.length; ++i) {
@@ -209,12 +147,17 @@ module.exports = function(arr, obj){
   return -1;
 };
 });
-require.register("component-classes/index.js", function(exports, require, module){
+
+require.register("component~classes@1.2.6", function (exports, module) {
 /**
  * Module dependencies.
  */
 
-var index = require('indexof');
+try {
+  var index = require('component~indexof@0.0.3');
+} catch (err) {
+  var index = require('component~indexof@0.0.3');
+}
 
 /**
  * Whitespace regexp.
@@ -248,7 +191,9 @@ module.exports = function(el){
  */
 
 function ClassList(el) {
-  if (!el) throw new Error('A DOM element reference is required');
+  if (!el || !el.nodeType) {
+    throw new Error('A DOM element reference is required');
+  }
   this.el = el;
   this.list = el.classList;
 }
@@ -324,26 +269,45 @@ ClassList.prototype.removeMatching = function(re){
 };
 
 /**
- * Toggle class `name`.
+ * Toggle class `name`, can force state via `force`.
+ *
+ * For browsers that support classList, but do not support `force` yet,
+ * the mistake will be detected and corrected.
  *
  * @param {String} name
+ * @param {Boolean} force
  * @return {ClassList}
  * @api public
  */
 
-ClassList.prototype.toggle = function(name){
+ClassList.prototype.toggle = function(name, force){
   // classList
   if (this.list) {
-    this.list.toggle(name);
+    if ("undefined" !== typeof force) {
+      if (force !== this.list.toggle(name, force)) {
+        this.list.toggle(name); // toggle again to correct
+      }
+    } else {
+      this.list.toggle(name);
+    }
     return this;
   }
 
   // fallback
-  if (this.has(name)) {
-    this.remove(name);
+  if ("undefined" !== typeof force) {
+    if (!force) {
+      this.remove(name);
+    } else {
+      this.add(name);
+    }
   } else {
-    this.add(name);
+    if (this.has(name)) {
+      this.remove(name);
+    } else {
+      this.add(name);
+    }
   }
+
   return this;
 };
 
@@ -355,7 +319,8 @@ ClassList.prototype.toggle = function(name){
  */
 
 ClassList.prototype.array = function(){
-  var str = this.el.className.replace(/^\s+|\s+$/g, '');
+  var className = this.el.getAttribute('class') || '';
+  var str = className.replace(/^\s+|\s+$/g, '');
   var arr = str.split(re);
   if ('' === arr[0]) arr.shift();
   return arr;
@@ -377,7 +342,8 @@ ClassList.prototype.contains = function(name){
 };
 
 });
-require.register("segmentio-extend/index.js", function(exports, require, module){
+
+require.register("segmentio~extend@1.0.0", function (exports, module) {
 
 module.exports = function extend (object) {
     // Takes an unlimited number of extenders.
@@ -394,10 +360,15 @@ module.exports = function extend (object) {
     return object;
 };
 });
-require.register("component-event/index.js", function(exports, require, module){
-var bind = (window.addEventListener !== undefined) ? 'addEventListener' : 'attachEvent',
-    unbind = (window.removeEventListener !== undefined) ? 'removeEventListener' : 'detachEvent',
-    prefix = (bind !== 'addEventListener') ? 'on' : '';
+
+require.register("component~event@0.2.0", function (exports, module) {
+var bind, unbind, prefix;
+
+function detect () {
+  bind = window.addEventListener ? 'addEventListener' : 'attachEvent';
+  unbind = window.removeEventListener ? 'removeEventListener' : 'detachEvent';
+  prefix = bind !== 'addEventListener' ? 'on' : '';
+}
 
 /**
  * Bind `el` event `type` to `fn`.
@@ -411,8 +382,8 @@ var bind = (window.addEventListener !== undefined) ? 'addEventListener' : 'attac
  */
 
 exports.bind = function(el, type, fn, capture){
+  if (!bind) detect();
   el[bind](prefix + type, fn, capture || false);
-
   return fn;
 };
 
@@ -428,13 +399,14 @@ exports.bind = function(el, type, fn, capture){
  */
 
 exports.unbind = function(el, type, fn, capture){
+  if (!unbind) detect();
   el[unbind](prefix + type, fn, capture || false);
-
   return fn;
 };
-});
-require.register("component-type/index.js", function(exports, require, module){
 
+});
+
+require.register("component~type@v1.2.1", function (exports, module) {
 /**
  * toString ref.
  */
@@ -451,25 +423,41 @@ var toString = Object.prototype.toString;
 
 module.exports = function(val){
   switch (toString.call(val)) {
-    case '[object Function]': return 'function';
     case '[object Date]': return 'date';
     case '[object RegExp]': return 'regexp';
     case '[object Arguments]': return 'arguments';
     case '[object Array]': return 'array';
-    case '[object String]': return 'string';
+    case '[object Error]': return 'error';
   }
 
   if (val === null) return 'null';
   if (val === undefined) return 'undefined';
+  if (val !== val) return 'nan';
   if (val && val.nodeType === 1) return 'element';
-  if (val === Object(val)) return 'object';
+
+  if (isBuffer(val)) return 'buffer';
+
+  val = val.valueOf
+    ? val.valueOf()
+    : Object.prototype.valueOf.apply(val);
 
   return typeof val;
 };
 
+// code borrowed from https://github.com/feross/is-buffer/blob/master/index.js
+function isBuffer(obj) {
+  return !!(obj != null &&
+    (obj._isBuffer || // For Safari 5-7 (missing Object.prototype.constructor)
+      (obj.constructor &&
+      typeof obj.constructor.isBuffer === 'function' &&
+      obj.constructor.isBuffer(obj))
+    ))
+}
+
 });
-require.register("timoxley-is-collection/index.js", function(exports, require, module){
-var typeOf = require('type')
+
+require.register("javve~is-collection@master", function (exports, module) {
+var typeOf = require('component~type@v1.2.1')
 
 /**
  * Evaluates _obj_ to determine if it's an array, an array-like collection, or
@@ -509,6 +497,19 @@ module.exports = function isCollection(obj) {
               return 2
             }
         } catch (ex) {}
+      case 'function':
+        if (isNodeList(obj)) return 2
+        try {
+          // indexed, but no tagName (element) or scrollTo/document (window. From DOM.isWindow test which we can't use here),
+          // or functions without apply/call (Safari
+          // HTMLElementCollection bug).
+          if ('length' in obj
+              && !obj.tagName
+            && !(obj.scrollTo && obj.document)
+            && !obj.apply) {
+              return 2
+            }
+        } catch (ex) {}
       default:
         return 0
     }
@@ -523,9 +524,10 @@ function isNodeList(nodes) {
 
 
 });
-require.register("javve-events/index.js", function(exports, require, module){
-var events = require('event'),
-  isCollection = require('is-collection');
+
+require.register("javve~events@v0.0.2", function (exports, module) {
+var events = require('component~event@0.2.0'),
+  isCollection = require('javve~is-collection@master');
 
 /**
  * Bind `el` event `type` to `fn`.
@@ -567,7 +569,8 @@ exports.unbind = function(el, type, fn, capture){
   }
 };
 });
-require.register("javve-get-by-class/index.js", function(exports, require, module){
+
+require.register("javve~get-by-class@v0.0.2", function (exports, module) {
 /**
  * Find all elements with class `className` inside `container`.
  * Use `single = true` to increase performance in older browsers
@@ -590,6 +593,7 @@ module.exports = (function() {
     };
   } else if (document.querySelector) {
     return function(container, className, single) {
+      className = '.' + className;
       if (single) {
         return container.querySelector(className);
       } else {
@@ -622,7 +626,8 @@ module.exports = (function() {
 })();
 
 });
-require.register("javve-to-string/index.js", function(exports, require, module){
+
+require.register("javve~to-string@v0.1.0", function (exports, module) {
 module.exports = function(s) {
     s = (s === undefined) ? "" : s;
     s = (s === null) ? "" : s;
@@ -631,17 +636,18 @@ module.exports = function(s) {
 };
 
 });
-require.register("list.fuzzysearch.js/index.js", function(exports, require, module){
-var classes = require('classes'),
-    events = require('events'),
-    extend = require('extend'),
-    toString = require('to-string'),
-    getByClass = require('get-by-class');
+
+require.register("list.fuzzysearch.js", function (exports, module) {
+var classes = require('component~classes@1.2.6'),
+    events = require('javve~events@v0.0.2'),
+    extend = require('segmentio~extend@1.0.0'),
+    toString = require('javve~to-string@v0.1.0'),
+    getByClass = require('javve~get-by-class@v0.0.2');
 
 module.exports = function(options) {
     options = options || {};
 
-    extend({
+    options = extend({
         location: 0,
         distance: 100,
         threshold: 0.4,
@@ -649,7 +655,7 @@ module.exports = function(options) {
         searchClass: 'fuzzy-search'
     }, options);
 
-    var fuzzy = require('./src/fuzzy'),
+    var fuzzy = require('list.fuzzysearch.js/src/fuzzy.js'),
         list;
 
     var fuzzySearch = {
@@ -707,7 +713,8 @@ module.exports = function(options) {
 };
 
 });
-require.register("list.fuzzysearch.js/src/fuzzy.js", function(exports, require, module){
+
+require.register("list.fuzzysearch.js/src/fuzzy.js", function (exports, module) {
 module.exports = function(text, pattern, options) {
     // Aproximately where in the text is the pattern expected to be found?
     var Match_Location = options.location || 0;
@@ -834,36 +841,11 @@ module.exports = function(text, pattern, options) {
 
 });
 
-
-
-
-
-
-require.alias("component-classes/index.js", "list.fuzzysearch.js/deps/classes/index.js");
-require.alias("component-classes/index.js", "classes/index.js");
-require.alias("component-indexof/index.js", "component-classes/deps/indexof/index.js");
-
-require.alias("segmentio-extend/index.js", "list.fuzzysearch.js/deps/extend/index.js");
-require.alias("segmentio-extend/index.js", "extend/index.js");
-
-require.alias("javve-events/index.js", "list.fuzzysearch.js/deps/events/index.js");
-require.alias("javve-events/index.js", "events/index.js");
-require.alias("component-event/index.js", "javve-events/deps/event/index.js");
-
-require.alias("timoxley-is-collection/index.js", "javve-events/deps/is-collection/index.js");
-require.alias("component-type/index.js", "timoxley-is-collection/deps/type/index.js");
-
-require.alias("javve-get-by-class/index.js", "list.fuzzysearch.js/deps/get-by-class/index.js");
-require.alias("javve-get-by-class/index.js", "get-by-class/index.js");
-
-require.alias("javve-to-string/index.js", "list.fuzzysearch.js/deps/to-string/index.js");
-require.alias("javve-to-string/index.js", "list.fuzzysearch.js/deps/to-string/index.js");
-require.alias("javve-to-string/index.js", "to-string/index.js");
-require.alias("javve-to-string/index.js", "javve-to-string/index.js");
-require.alias("list.fuzzysearch.js/index.js", "list.fuzzysearch.js/index.js");if (typeof exports == "object") {
+if (typeof exports == "object") {
   module.exports = require("list.fuzzysearch.js");
 } else if (typeof define == "function" && define.amd) {
-  define(function(){ return require("list.fuzzysearch.js"); });
+  define("ListFuzzySearch", [], function(){ return require("list.fuzzysearch.js"); });
 } else {
-  this["ListFuzzySearch"] = require("list.fuzzysearch.js");
-}})();
+  (this || window)["ListFuzzySearch"] = require("list.fuzzysearch.js");
+}
+})()
